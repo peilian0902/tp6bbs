8a39f366a7c260e99db191447f126e72b0e007c0
peilian <455302716@qq.com>
2020-08-06 10:07:23 +0800

    前端布局模板

---
+++ app/middleware/Check.php
@@ -0,0 +1,24 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\middleware;
+
+class Check
+{
+    /**
+     * 处理请求
+     *
+     * @param \think\Request $request
+     * @param \Closure       $next
+     * @return Response
+     */
+    public function handle($request, \Closure $next, $name)
+    {
+        //
+        if ($name == 'think') {
+            return redirect('index/think');
+        }
+
+        return $next($request);
+    }
+}
